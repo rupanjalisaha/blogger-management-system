1cd7471aa454db3e520821532098d89c31605b5c
1000098423 <rupanjalisaha2001@gmail.com>
2026-04-14 20:08:30 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -94,7 +94,7 @@ export default function Home() {
         <h1 style={{ fontFamily:"sans-serif", marginTop: "2%", marginBottom:"3%" }}>
           UVB Portal – Explore, Write, and Share Space Technology
         </h1>
-        <p style={{ fontFamily: "monospace", fontSize: "18px", padding:"5px", textAlign:"justify", marginBottom:"4%", lineHeight:"1cm" }}>
+        <p style={{ fontFamily: "monospace", fontSize: "18px", padding:"5px", textAlign:"justify", marginBottom:"4%", lineHeight:"1cm", color:"AppWorkspace" }}>
           UVB Portal is a modern blogging platform designed for beginner-level
           writers passionate about space technology. It provides a secure and
           intuitive environment where users can create, manage, and share
